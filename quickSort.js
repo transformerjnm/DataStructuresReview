38d654c5b724ae34c7571e6cc7447737606e858e
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-06-01 21:38:43 -0400

    Update quickSort.js

--- quickSort.js
+++ quickSort.js
@@ -12,7 +12,7 @@ quickSort = array => {
         return array;
     }
 
-    //set pivot as last value
+    //set pivot as last value in array
     const pivot = array[ array.length - 1 ];
     const leftArray = [];
     const rightArray = [];
@@ -20,12 +20,14 @@ quickSort = array => {
     //go though values in array. if value is less than pivot put value in left array else put in right array
     for ( let i = 0; i < array.length -1; i++ ) {
 
+        //left array. value is less
         if ( array[ i ] < pivot ){
 
             leftArray.push( array[ i ] );
 
         } else {
 
+            //right array value is greater
             rightArray.push( array[ i ] );
 
         }
@@ -35,20 +37,23 @@ quickSort = array => {
     //keep calling this function until all items are sorted. then the function returns the order array.
     if ( leftArray.length > 0 && rightArray.length > 0 ) { 
 
+        //we have a right and left array. (values are in both arrays ). recursion call both arrays
         return [ ...quickSort( leftArray ) , pivot, ...quickSort( rightArray ) ];
 
     } else if ( leftArray.length > 0 ) {
 
+        //we only have a left array. recursion on left array
         return [ ...quickSort( leftArray ) , pivot ];
 
     } else {
 
+        //only have a right array recursion on right array
         return [ pivot, ...quickSort( rightArray ) ];
 
-    }
+    }//if array.length do some recursion
 
 
-}
+}//quicksort function
 
 //test array
-console.log(quickSort([1, 4, 2, 8, 345, 123, 43, 6543]));
+console.log( quickSort( [ 1, 4, 2, 8, 345, 123, 43, 6543]  ) );
